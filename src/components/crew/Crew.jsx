89f3187851cd44d754bfad875795c218134744b1
linavaquero <lina.vaquero2017@gmail.com>
2023-06-05 12:42:59 -0500

    adaptacion con database

--- src/components/crew/Crew.jsx
+++ src/components/crew/Crew.jsx
@@ -9,20 +9,20 @@ const CrewCarousel = ({ crew }) => {
     <>
       <div className='main_container_principal'>
         <div className='main_main'>
-          <p className='main_numTitle'>{crew.numTitle}</p>
-          <h2 className='main_title'>{crew.title}</h2>
+          <p className='main_numTitle'>{crew?.numTitle}</p>
+          <h2 className='main_title'>{crew?.title}</h2>
         </div>
 
         <div className='main_container'>
           <div className='main_description'>
-            <h3 className='main_profession'>{crew.Profession}</h3>
-            <h2 className='main_name'>{crew.name}</h2>
-            <p className='main_paragraph'>{crew.paragraph}</p>
+            <h3 className='main_profession'>{crew?.Profession}</h3>
+            <h2 className='main_name'>{crew?.name}</h2>
+            <p className='main_paragraph'>{crew?.paragraph}</p>
           </div>
 
           <div className='main_figure'>
             <figure className='main_foto1'>
-              <img src={crew.imagen} alt={crew.name} />
+            <img src={crew?.images?.png} alt={crew?.name} />
             </figure>
           </div>
         </div>
