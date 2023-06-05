5f6eb27778f0c6fc62a516fdada87b4efa4f8fbb
linavaquero <lina.vaquero2017@gmail.com>
2023-06-05 12:46:03 -0500

    correcion de datos

--- src/components/crew/CrewCarouse.jsx
+++ src/components/crew/CrewCarouse.jsx
@@ -1,64 +1,29 @@
-import React, { useDeferredValue, useEffect, useState } from 'react'
-import douglas from '../../assets/img/image-douglas.svg'
-import mark from '../../assets/img/image-Marks.svg'
-import victor from '../../assets/img/image-Victor.svg'
-import ansari from '../../assets/img/image-Ansari.svg'
+import React, { useEffect, useState } from 'react'
+
 import './styleCrew.scss'
 import CrewCarousel from './Crew'
 import ImagenCrew from '../../assets/img/Bitmap.png'
+import { getCrew } from '../../service/getCrew'
 
 const Crew = () => {
   const [activeSlide, setActiveSlide] = useState(0);
   const [crewDescription, setCrewDescription] = useState({});
-  const crewData = [
-    {
-      "id": 1,
-      "name": "DOUGLAS HURLEY",
-      "Profession": "COMMANDER",
-      "imagen": douglas,
-      "numTitle": "02",
-      "title": "MEET YOUR CREW",
-      "paragraph": "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
-    },
-    {
-      "id": 2,
-      "name": "MARK SHUTTLEWORTH",
-      "Profession": "Mission Specialist",
-      "imagen": mark,
-      "numTitle": "02",
-      "title": "MEET YOUR CREW",
-      "paragraph": "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist. ",
-    },
-    {
-      "id": 3,
-      "name": "VICTOR GLOVER",
-      "Profession": "PILOT",
-      "imagen": victor,
-      "numTitle": "02",
-      "title": "MEET YOUR CREW",
-      "paragraph": "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
-    },
-    {
-      "id": 4,
-      "name": "ANOUSHEH ANSARI",
-      "Profession": "FLIGHT ENGINNER",
-      "imagen": ansari,
-      "numTitle": "02",
-      "title": "MEET YOUR CREW",
-      "paragraph": "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.  ",
-    }]
-  const handleSlideChange = (index) => {
-    setActiveSlide(index);
-   
-    const crew = crewData[index]
-    setCrewDescription(crew)
-
-  };
+  console.log(crewDescription);
+  const[crewData,setCrewData ] =useState([0])
+  
+  const getData = async () =>{
+    const data = await getCrew();
+    setCrewData (data)
+  }
   useEffect(() => {
-    handleSlideChange(0)
+  
+    getData()
+   
   }, []);
 
+
   useEffect(() => {
+     handleSlideChange(0)
     document.body.style.background = `url(${ImagenCrew}) no-repeat center center fixed`;
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundColor = '#0B0D17';
@@ -67,6 +32,14 @@ const Crew = () => {
     };
   },
   );
+
+  const handleSlideChange = (index) => {
+    setActiveSlide(index);
+   
+    const crew = crewData[index]
+    setCrewDescription(crew)
+
+  };
 
   return (
     <div className='main_crew'>
@@ -92,3 +65,5 @@ const Crew = () => {
 }
 
 export default Crew
+
+
